6014a5fe8f205320735aa9f70293e687a9b31d98
dwm <dwm@mcplex.net>
2026-01-07 23:58:40 -0500

    Trivial test of Config

---
+++ classes/tests/TestConfig.cc
@@ -0,0 +1,56 @@
+//===========================================================================
+//  Copyright (c) Daniel W. McRobb 2026
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions
+//  are met:
+//
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//  3. The names of the authors and copyright holders may not be used to
+//     endorse or promote products derived from this software without
+//     specific prior written permission.
+//
+//  IN NO EVENT SHALL DANIEL W. MCROBB BE LIABLE TO ANY PARTY FOR
+//  DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES,
+//  INCLUDING LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE,
+//  EVEN IF DANIEL W. MCROBB HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH
+//  DAMAGE.
+//
+//  THE SOFTWARE PROVIDED HEREIN IS ON AN "AS IS" BASIS, AND
+//  DANIEL W. MCROBB HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT,
+//  UPDATES, ENHANCEMENTS, OR MODIFICATIONS. DANIEL W. MCROBB MAKES NO
+//  REPRESENTATIONS AND EXTENDS NO WARRANTIES OF ANY KIND, EITHER
+//  IMPLIED OR EXPRESS, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE,
+//  OR THAT THE USE OF THIS SOFTWARE WILL NOT INFRINGE ANY PATENT,
+//  TRADEMARK OR OTHER RIGHTS.
+//===========================================================================
+
+//---------------------------------------------------------------------------
+//!  @file TestConfig.cc
+//!  @author Daniel W. McRobb
+//!  @brief NOT YET DOCUMENTED
+//---------------------------------------------------------------------------
+
+#include "DwmMclogConfig.hh"
+
+//----------------------------------------------------------------------------
+//!  
+//----------------------------------------------------------------------------
+int main(int argc, char *argv[])
+{
+  Dwm::Mclog::Config  cfg;
+  
+  assert(cfg.Parse("../../etc/mclogd.cfg"));
+  assert(cfg.service.keyDirectory == "/usr/local/etc/mclogd");
+  assert(cfg.mcast.groupAddr == Dwm::Ipv4Address("239.240.241.242"));
+  assert(cfg.mcast.intfAddr == Dwm::Ipv4Address("192.168.168.57"));
+  assert(cfg.mcast.dstPort == 3456);
+  
+  return 0;
+}
