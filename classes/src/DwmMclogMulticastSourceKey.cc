162de001568fe986adc1f502db98ce77a5caa4e5
dwm <dwm@mcplex.net>
2026-02-05 00:54:19 -0500

    Add @brief comment

--- classes/src/DwmMclogMulticastSourceKey.cc
+++ classes/src/DwmMclogMulticastSourceKey.cc
@@ -34,7 +34,7 @@
 //---------------------------------------------------------------------------
 //!  @file DwmMclogMulticastSourceKey.cc
 //!  @author Daniel W. McRobb
-//!  @brief NOT YET DOCUMENTED
+//!  @brief Dwm::Mclog::MulticastSourceKey implementation
 //---------------------------------------------------------------------------
 
 #include "DwmMclogMulticastSourceKey.hh"
