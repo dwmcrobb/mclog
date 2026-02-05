9c0cbb3cba5e67b0429c8fc05b1fe91158c87d2b
dwm <dwm@mcplex.net>
2026-02-05 00:39:13 -0500

    Add @brief comment

--- classes/src/DwmMclogKeyRequestClientState.cc
+++ classes/src/DwmMclogKeyRequestClientState.cc
@@ -34,7 +34,7 @@
 //---------------------------------------------------------------------------
 //!  @file DwmMcLogKeyRequestClientState.cc
 //!  @author Daniel W. McRobb
-//!  @brief NOT YET DOCUMENTED
+//!  @brief Dwm::Mclog::KeyRequestClient state implementation
 //---------------------------------------------------------------------------
 
 #include <cstring>
