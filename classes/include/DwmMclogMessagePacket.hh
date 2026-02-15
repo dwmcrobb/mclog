aaba1162376597aacbc8a3023b708df5b57b9e16
dwm <dwm@mcplex.net>
2026-02-14 20:00:31 -0500

    Add some comments

--- classes/include/DwmMclogMessagePacket.hh
+++ classes/include/DwmMclogMessagePacket.hh
@@ -34,7 +34,7 @@
 //---------------------------------------------------------------------------
 //!  @file DwmMclogMessagePacket.hh
 //!  @author Daniel W. McRobb
-//!  @brief NOT YET DOCUMENTED
+//!  @brief Dwm::Mclog::MessagePacket class declaration
 //---------------------------------------------------------------------------
 
 #ifndef _DWMMCLOGMESSAGEPACKET_HH_
@@ -70,7 +70,10 @@ namespace Dwm {
   namespace Mclog {
 
     //------------------------------------------------------------------------
-    //!  
+    //!  Encapsulates a buffer containing one or more messages, in the form
+    //!  used for network transport (including bytes needed for encryption
+    //!  and AEAD).  Note that MessagePacket does NOT own the buffer and
+    //!  does not participate in the lifetime of the buffer.
     //------------------------------------------------------------------------
     class MessagePacket
     {
@@ -80,7 +83,10 @@ namespace Dwm {
       static const size_t k_minPacketLen = k_nonceLen + k_macLen;
 
       //----------------------------------------------------------------------
-      //!  
+      //!  Construct from the buffer @c buf of length @c buflen.  Note that
+      //!  MessagePacket does NOT participate in the lifetime of the buffer;
+      //!  the caller is responsible for ensuring it remains valid as long
+      //!  as it might be referenced by the MessagePacket.
       //----------------------------------------------------------------------
       MessagePacket(char *buf, size_t buflen)
           : _buf(buf), _buflen(buflen),
@@ -90,7 +96,9 @@ namespace Dwm {
       { assert(_buf && (_buflen > k_minPacketLen)); }
 
       //----------------------------------------------------------------------
-      //!  
+      //!  If it will fit, @c t is appended to the payload and @c true is
+      //!  returned.  If it will not fit, @c t is not appended and @c false
+      //!  is returned.
       //----------------------------------------------------------------------
       template <typename T>
       bool Add(const T & t)
@@ -110,61 +118,79 @@ namespace Dwm {
       }
 
       //----------------------------------------------------------------------
-      //!  
+      //!  Send the packet to the given destination @c dst via the given
+      //!  descriptor @c fd, using the given @c secretKey to encrypt the
+      //!  packet.  Returns the number of bytes sent (positive on success).
       //----------------------------------------------------------------------
       ssize_t SendTo(int fd, const std::string & secretKey,
                      const UdpEndpoint & dst);
 
       //----------------------------------------------------------------------
-      //!  
+      //!  Send the packet to the given destination @c dst via the given
+      //!  descriptor @c fd, unencrypted.  This is used by local loggers
+      //!  sending to the loopback address.  Returns the number of bytes sent
+      //!  (positive on success).
       //----------------------------------------------------------------------
       ssize_t SendTo(int fd, const UdpEndpoint & dst);
 
       //----------------------------------------------------------------------
-      //!  
+      //!  Receives the packet via the given descriptor @c fd, decrypts it
+      //!  using the given @c secretKey and populates @c src with the source
+      //!  of the packet.  Returns the number of bytes received (positive on
+      //!  success).
       //----------------------------------------------------------------------
       ssize_t RecvFrom(int fd, const std::string & secretKey,
                        struct sockaddr_in *src);
 
       //----------------------------------------------------------------------
-      //!  
+      //!  Receives the packet via the given descriptor @c fd, decrypts it
+      //!  using the given @c secretKey and populates @c src with the source
+      //!  of the packet.  Returns the number of bytes received (positive on
+      //!  success).
       //----------------------------------------------------------------------
       ssize_t RecvFrom(int fd, const std::string & secretKey,
                        struct sockaddr_in6 *src);
       
       //----------------------------------------------------------------------
-      //!  
+      //!  Receives the packet (unencrypted) via the given descriptor @c fd
+      //!  and populates @c src with the source of the packet.  Returns the
+      //!  number of bytes received (positive on success).
       //----------------------------------------------------------------------
       ssize_t RecvFrom(int fd, struct sockaddr_in *src);
 
       //----------------------------------------------------------------------
-      //!  
+      //!  Receives the packet (unencrypted) via the given descriptor @c fd
+      //!  and populates @c src with the source of the packet.  Returns the
+      //!  number of bytes received (positive on success).
       //----------------------------------------------------------------------
       ssize_t RecvFrom(int fd, struct sockaddr_in6 *src);
 
       //----------------------------------------------------------------------
-      //!  
+      //!  Encrypts the packet with the given @c secretKey.  Returns true
+      //!  on success, false on failure.
       //----------------------------------------------------------------------
       bool Encrypt(const std::string & secretKey);
 
       //----------------------------------------------------------------------
-      //!  
+      //!  Clears the payload of the packet.
       //----------------------------------------------------------------------
       void Reset();
       
       //----------------------------------------------------------------------
-      //!  
+      //!  Decrypts the packet (in-place) of length @c recvlen using the
+      //!  given @c secretKey.  Returns the number of bytes decrypted
+      //!  (@c recvlen on success, -1 on failure).
       //----------------------------------------------------------------------
       ssize_t Decrypt(size_t recvlen, const std::string & secretKey);
       
       //----------------------------------------------------------------------
-      //!  Does this always work?
+      //!  Returns true if the packet has a non-empty payload.
       //----------------------------------------------------------------------
       bool HasPayload()
       { return _payloadLength > 0; }
 
       //----------------------------------------------------------------------
-      //!  
+      //!  Returns the payload.
       //----------------------------------------------------------------------
       std::spanstream & Payload()
       { return _payload; }
